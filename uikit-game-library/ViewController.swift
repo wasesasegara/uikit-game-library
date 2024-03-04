5ea60ce3545192bbcdac225b20ee9c2e1825684d
wasesasegara <wasesasegara@gmail.com>
2024-03-05 01:03:10 +0700

    Init procject files

---
+++ uikit-game-library/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-game-library
+//
+//  Created by Bisma S Wasesasegara on 05/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
